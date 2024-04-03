80a93004f7c56d8bc4e6b240b4d5f0492e5bf2d9
duydq2 <duydq2@vng.com.vn>
2024-04-04 00:02:48 +0700

    commit

--- src/main/java/org/example/daily_challenge/April2024.java
+++ src/main/java/org/example/daily_challenge/April2024.java
@@ -9,6 +9,41 @@ public class April2024 {
         return parts[parts.length - 1].length();
     }
 
+    // 79. Word Search
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word.charAt(0) == board[i][j] && search(board, i, j, 0, visited, word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean search(char[][] board, int i, int j, int index, boolean[][] visited, String word) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]) {
+            return false;
+        }
+
+        if (word.charAt(index) != board[i][j]) return false;
+        if (index == word.length() - 1) return true;
+
+        visited[i][j] = true;
+        if (search(board, i + 1, j, index + 1, visited, word)
+                || search(board, i - 1, j, index + 1, visited, word)
+                || search(board, i, j + 1, index + 1, visited, word)
+                || search(board, i, j - 1, index + 1, visited, word)) {
+            return true;
+        }
+        visited[i][j] = false;
+        return false;
+    }
+
     // 205. Isomorphic Strings
     public boolean isIsomorphic(String s, String t) {
         HashMap<Character, Character> map = new HashMap<>();
