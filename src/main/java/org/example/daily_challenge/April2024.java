93ab2dc1f80c7242dd86a4b3b3a0fa02fdfbf667
duy <dragonflyer1998@gmail.com>
2024-04-24 21:59:38 +0700

    commit

--- src/main/java/org/example/daily_challenge/April2024.java
+++ src/main/java/org/example/daily_challenge/April2024.java
@@ -361,6 +361,20 @@ public class April2024 {
         return null;
     }
 
+    // 1137. N-th Tribonacci Number
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int[] memo = new int[n + 1];
+        memo[1] = 1; memo[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+        }
+        return memo[n];
+    }
+
     // 1249. Minimum Remove to Make Valid Parentheses
     /* ---- Stack Method ---- */
     public String minRemoveToMakeValid(String s) {
