8f120f73f5287c28f9a624fdf97cea48fb2cdac5
Ev4LiA <dragonflyer1998@gmail.com>
2024-05-01 21:24:53 +0700

    commit

---
+++ src/main/java/org/example/daily_challenge/May2024.java
@@ -0,0 +1,15 @@
+package org.example.daily_challenge;
+
+public class May2024 {
+    // 2000. Reverse Prefix of Word
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) {
+            return word;
+        } else {
+            StringBuilder sb = new StringBuilder();
+            String sub = sb.append(word, 0, index + 1).reverse().toString();
+            return sub + word.substring(index + 1);
+        }
+    }
+}
