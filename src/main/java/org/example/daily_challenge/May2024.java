911ff30692f2cf7ad1a07a66e24faa749d06aa5e
duy <dragonflyer1998@gmail.com>
2024-05-03 16:20:56 +0700

    commit

--- src/main/java/org/example/daily_challenge/May2024.java
+++ src/main/java/org/example/daily_challenge/May2024.java
@@ -1,6 +1,25 @@
 package org.example.daily_challenge;
 
+import java.util.Arrays;
+
 public class May2024 {
+    // 165. Compare Version Numbers
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for (int i = 0; i < Math.max(v1.length, v2.length); i++) {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
     // 2000. Reverse Prefix of Word
     public String reversePrefix(String word, char ch) {
         int index = word.indexOf(ch);
@@ -11,5 +30,21 @@ public class May2024 {
             String sub = sb.append(word, 0, index + 1).reverse().toString();
             return sub + word.substring(index + 1);
         }
+    }
+
+    // 2441. Largest Positive Integer That Exists With Its Negative
+    public int findMaxK(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
+        while (nums[i] < 0 && i < j) {
+            if (nums[j] == -nums[i]) {
+                return nums[j];
+            } else if (nums[j] > -nums[i]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return -1;
     }
 }
