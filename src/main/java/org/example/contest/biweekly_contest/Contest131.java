1730d1b8c569469d41d28f2048f6d70786f5a6f8
duy <dragonflyer1998@gmail.com>
2024-05-28 09:42:08 +0700

    commit

--- src/main/java/org/example/contest/biweekly_contest/Contest131.java
+++ src/main/java/org/example/contest/biweekly_contest/Contest131.java
@@ -3,6 +3,6 @@ package org.example.contest.biweekly_contest;
 public class Contest131 {
     // 3158. Find the XOR of Numbers Which Appear Twice
     public int duplicateNumbersXOR(int[] nums) {
-
+        return 0;
     }
 }
