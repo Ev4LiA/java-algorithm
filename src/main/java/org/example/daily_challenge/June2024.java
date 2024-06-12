eacc8d60c6abb9f925198303d8e5a0767e4cbaaf
duy <dragonflyer1998@gmail.com>
2024-06-12 16:55:46 +0700

    commit

--- src/main/java/org/example/daily_challenge/June2024.java
+++ src/main/java/org/example/daily_challenge/June2024.java
@@ -5,6 +5,33 @@ import org.example.utilities.TrieNode;
 import java.util.*;
 
 public class June2024 {
+    // 75. Sort Colors
+    public void sortColors(int[] nums) {
+        int red = 0, white = 0, blue = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                red++;
+            } else if (nums[i] == 1) {
+                white++;
+            } else if (nums[i] == 2) {
+                blue++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (red > 0) {
+                nums[i] = 0;
+                red--;
+            } else if (white > 0) {
+                nums[i] = 1;
+                white--;
+            } else {
+                nums[i] = 2;
+                blue--;
+            }
+        }
+    }
+
     // 198. House Robber
     public int rob(int[] nums) {
         int n = nums.length;
