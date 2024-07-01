d84782c6f8bb00ba734942d6bf3374c318a03df4
duy <dragonflyer1998@gmail.com>
2024-07-01 23:16:05 +0700

    commit

---
+++ src/main/java/org/example/daily_challenge/July2024.java
@@ -0,0 +1,17 @@
+package org.example.daily_challenge;
+
+public class July2024 {
+    // 1550. Three Consecutive Odds
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count == 3) return true;
+        }
+        return false;
+    }
+}
