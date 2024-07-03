35ec879e993bb7397259cdb78b764463a7f14db8
duy <dragonflyer1998@gmail.com>
2024-07-03 14:22:22 +0700

    commit

--- src/main/java/org/example/daily_challenge/July2024.java
+++ src/main/java/org/example/daily_challenge/July2024.java
@@ -1,6 +1,20 @@
 package org.example.daily_challenge;
 
+import java.util.Arrays;
+
 public class July2024 {
+    // 1509. Minimum Difference Between Largest and Smallest Value in Three Moves
+    public int minDifference(int[] nums) {
+        Arrays.sort(nums);
+        int max = Integer.MAX_VALUE, n = nums.length;
+        if (n <= 3) return 0;
+        max = Math.min(max, nums[n - 1] - nums[3]);
+        max = Math.min(max, nums[n - 2] - nums[2]);
+        max = Math.min(max, nums[n - 3] - nums[1]);
+        max = Math.min(max, nums[n - 4] - nums[0]);
+        return max;
+    }
+
     // 1550. Three Consecutive Odds
     public boolean threeConsecutiveOdds(int[] arr) {
         int count = 0;
